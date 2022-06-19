59fa889cbdb759e6c4479a57366bcc9ef6f95f01
alex kimathi <keemalekie@gmail.com>
2022-06-19 11:28:40 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+function createEmployeeRecord(array){
+    const records ={
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return records
+}
+
+function createEmployeeRecords(array){
+    let newArr = []
+    array.forEach(arr=>{
+        newArr.push(createEmployeeRecord(arr))
+    })
+    return newArr
+}
+
+function createTimeInEvent(){
+
+}
+
+
+function createTimeInEvent(){
+ 
+}
+
+function hoursWorkedOnDate() {
+
+}
+
+function wagesEarnedOnDate(){
+
+}
+
+function allWagesFor(){
+
+}
+function calculatePayroll(){
+  
+}
